83376bcc7578188978527ecc30038ca92a050557
David Ricci <121657626+dddk1ddd@users.noreply.github.com>
2023-02-08 05:23:15 -0500

    Add files via upload

--- psniff.py
+++ psniff.py
@@ -2,25 +2,30 @@ import subprocess
 
 
 def _main():
-    while True:
-        command = input("Enter command (list, start, etl2txt, exit): ")
+    try:
+        while True:
+            command = input("Enter command (list, start, etl2txt, exit): ")
 
-        if command == "start":
-            device = input("Enter device number: ")
-            if not device:
-                device = 1
-            print("Monitoring device " + device + " in Realtime\n\n")
-            subprocess.run(["pktmon", "start", "-c", "--comp", device, "-m", "real-time"])
-        elif command == "etl2txt":
-            print("Converting PktMon.etl to PktMon.txt\n\n")
-            subprocess.run(["pktmon", "etl2txt", "C:\\Windows\\system32\\PktMon.etl"])
-        elif command == "list":
-            print("Listing devices\n\n")
-            subprocess.run(["pktmon", "list"])
-        elif command == "exit":
-            return
-        else:
-            print("Invalid argument.")
+            if command == "start":
+                device = input("Enter device number: ")
+                if not device:
+                  device = 1
+                print("Monitoring device " + device + " in Realtime\n\n")
+                subprocess.run(["pktmon", "start", "-c", "--trace",
+                                "-p", "Microsoft-Windows-TCPIP", "--comp", device, "-m", "real-time"])
+            elif command == "etl2txt":
+                print("Converting PktMon.etl to PktMon.txt\n\n")
+                subprocess.run(["pktmon", "etl2txt", "C:\\Windows\\system32\\PktMon.etl"])
+            elif command == "list":
+                print("Listing devices\n\n")
+                subprocess.run(["pktmon", "list"])
+            elif command == "exit":
+               return
+            else:
+                print("Invalid argument.")
+    except KeyboardInterrupt:
+        print("\nStopping packet monitoring\n\n")
+        subprocess.run(["pktmon", "stop"])
 
 
 if __name__ != "__main__":
