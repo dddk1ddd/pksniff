d69243cedc8640547bd90b81c0775833a9195e5a
David Ricci <121657626+dddk1ddd@users.noreply.github.com>
2023-02-08 05:35:26 -0500

    Add files via upload

--- psniff.py
+++ psniff.py
@@ -4,7 +4,7 @@ import subprocess
 def _main():
     try:
         while True:
-            command = input("Enter command (list, start, etl2txt, exit): ")
+            command = input("Enter command (list, start, exit): ")
 
             if command == "start":
                 device = input("Enter device number: ")
@@ -13,19 +13,18 @@ def _main():
                 print("Monitoring device " + device + " in Realtime\n\n")
                 subprocess.run(["pktmon", "start", "-c", "--trace",
                                 "-p", "Microsoft-Windows-TCPIP", "--comp", device, "-m", "real-time"])
-            elif command == "etl2txt":
-                print("Converting PktMon.etl to PktMon.txt\n\n")
-                subprocess.run(["pktmon", "etl2txt", "C:\\Windows\\system32\\PktMon.etl"])
             elif command == "list":
                 print("Listing devices\n\n")
                 subprocess.run(["pktmon", "list"])
             elif command == "exit":
-               return
+                return
             else:
                 print("Invalid argument.")
     except KeyboardInterrupt:
         print("\nStopping packet monitoring\n\n")
         subprocess.run(["pktmon", "stop"])
+        print("Converting PktMon.etl to PktMon.txt\n\n")
+        subprocess.run(["pktmon", "etl2txt", "C:\\Windows\\system32\\PktMon.etl"])
 
 
 if __name__ != "__main__":
