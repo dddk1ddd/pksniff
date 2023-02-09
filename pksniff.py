4f46a17bec9d602018d9bf9fc06669c7eda2e697
David Ricci <121657626+dddk1ddd@users.noreply.github.com>
2023-02-09 09:06:01 -0500

    Add files via upload

--- pksniff.py
+++ pksniff.py
@@ -2,29 +2,95 @@ import subprocess
 
 
 def _main():
+    print("\n *** WARNING! This script will remove" +
+          "\n *** any PktMon filters you have enabled." +
+          "\n *** Exit now to avoid losing filters.\n")
     try:
         while True:
-            command = input("Enter command (list, start, exit): ")
+            command = input("Enter command:\n\n dns\n tcp\n dhcp\n ftp\n telnet\n icmp\n smb\n device\n list\n lookup\n exit\n\n? ")
 
-            if command == "start":
-                device = input("Enter device number: ")
+            if command == "lookup":
+                addy = input("\nAddress? ")
+                subprocess.run(["nslookup", addy])
+            if command == "dhcp":
+                subprocess.run(["pktmon", "filter", "remove"])
+                subprocess.run(["pktmon", "filter", "add", "DhcpTraffic", "-t", "UDP", "-p", "67"])
+                subprocess.run(["pktmon", "filter", "add", "DhcpTraffic", "-t", "UDP", "-p", "68"])
+                print("Monitoring...\n\n")
+                try:
+                    subprocess.run(["pktmon", "start", "-c", "-m", "real-time"])
+                except KeyboardInterrupt:
+                    pass
+            if command == "smb":
+                    subprocess.run(["pktmon", "filter", "remove"])
+                    subprocess.run(["pktmon", "filter", "add", "SMB", "-t", "TCP", "SYN", "-p", "445"])
+                    print("Monitoring...\n\n")
+                    try:
+                        subprocess.run(["pktmon", "start", "-c", "-m", "real-time"])
+                    except KeyboardInterrupt:
+                        pass
+            if command == "icmp":
+                subprocess.run(["pktmon", "filter", "remove"])
+                subprocess.run(["pktmon", "filter", "add", "Ping", "-t", "ICMP"])
+                print("Monitoring...\n\n")
+                try:
+                    subprocess.run(["pktmon", "start", "-c", "-m", "real-time"])
+                except KeyboardInterrupt:
+                    pass
+            if command == "ftp":
+                subprocess.run(["pktmon", "filter", "remove"])
+                subprocess.run(["pktmon", "filter", "add", "FTP", "-t", "TCP", "-p", "21"])
+                print("Monitoring...\n\n")
+                try:
+                    subprocess.run(["pktmon", "start", "-c", "-m", "real-time"])
+                except KeyboardInterrupt:
+                    pass
+            if command == "telnet":
+                subprocess.run(["pktmon", "filter", "remove"])
+                subprocess.run(["pktmon", "filter", "add", "Telnet", "-t", "TCP", "-p", "23"])
+                print("Monitoring...\n\n")
+                try:
+                    subprocess.run(["pktmon", "start", "-c", "-m", "real-time"])
+                except KeyboardInterrupt:
+                    pass
+            if command == "dns":
+                subprocess.run(["pktmon", "filter", "remove"])
+                subprocess.run(["pktmon", "filter", "add", "DnsTraffic", "-t", "UDP", "-p", "53"])
+                print("Monitoring...\n\n")
+                try:
+                    subprocess.run(["pktmon", "start", "-c", "-m", "real-time"])
+                except KeyboardInterrupt:
+                    pass
+            if command == "tcp":
+                subprocess.run(["pktmon", "filter", "remove"])
+                subprocess.run(["pktmon", "filter", "add", "WebTraffic", "-t", "TCP", "-p", "443", "-d", "IPv4"])
+                subprocess.run(["pktmon", "filter", "add", "WebTraffic", "-t", "TCP", "-p", "80", "-d", "IPv4"])
+                print("Monitoring...\n\n")
+                try:
+                    subprocess.run(["pktmon", "start", "-c", "-m", "real-time"])
+                except KeyboardInterrupt:
+                    pass
+            if command == "device":
+                subprocess.run(["pktmon", "filter", "remove"])
+                device = input("\nEnter device number (default: all)? ")
                 if not device:
-                  device = 1
-                print("Monitoring device " + device + " in Realtime\n\n")
-                subprocess.run(["pktmon", "start", "-c", "--trace",
-                                "-p", "Microsoft-Windows-TCPIP", "--comp", device, "-m", "real-time"])
+                    device = "all"
+                print("Monitoring device " + device + "...\n\n")
+                try:
+                    subprocess.run(["pktmon", "start", "-c", "--trace",
+                                    "-p", "Microsoft-Windows-TCPIP", "--comp", device,
+                                    "-m", "real-time"])
+                except KeyboardInterrupt:
+                    pass
             elif command == "list":
-                print("Listing devices\n\n")
+                print("Listing devices...\n\n")
                 subprocess.run(["pktmon", "list"])
             elif command == "exit":
                 return
             else:
-                print("Invalid argument.")
+                pass
     except KeyboardInterrupt:
-        print("\nStopping packet monitoring\n\n")
-        subprocess.run(["pktmon", "stop"])
-        print("Converting PktMon.etl to PktMon.txt\n\n")
-        subprocess.run(["pktmon", "etl2txt", "C:\\Windows\\system32\\PktMon.etl"])
+        print("\nStopping packet monitoring...\n\n")
 
 
 if __name__ != "__main__":
